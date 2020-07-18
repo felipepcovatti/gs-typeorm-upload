444f2db3828d0b35f26817eda4b8c66b4c327f15
Felipe P Covatti <felipepcovatti@gmail.com>
2020-07-18 11:51:07 -0300

    Implement transaction creation route and service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category: category_title,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    const existingCategory = await categoryRepository.findOne({
+      where: { title: category_title },
+    });
+
+    let category_id = '';
+    if (existingCategory) {
+      category_id = existingCategory.id;
+    } else {
+      const category = categoryRepository.create({
+        title: category_title,
+      });
+
+      await categoryRepository.save(category);
+
+      category_id = category.id;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
