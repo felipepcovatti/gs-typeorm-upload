09957b2dd9a078f242a4426feba387fd535d9ab1
Felipe P Covatti <felipepcovatti@gmail.com>
2020-07-18 19:29:34 -0300

    Implement transactions listing route

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,25 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (acc, el) => {
+        acc[el.type] += el.value;
+
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
