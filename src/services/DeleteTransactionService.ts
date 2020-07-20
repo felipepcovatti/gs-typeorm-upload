f76eaf6575f0f87ada264d0e05ed6f6c53192042
Felipe P Covatti <felipepcovatti@gmail.com>
2020-07-20 09:14:14 -0300

    Set route and service for deleting a transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne({
+      where: { id },
+    });
+
+    if (transaction) {
+      await transactionsRepository.delete(id);
+    } else {
+      throw new AppError('Entity not found');
+    }
   }
 }
 
